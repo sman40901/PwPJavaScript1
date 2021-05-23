48ec749426d1b4cc095537e597a8c774b6b2ac09
sman40901 <sujanmanpr@gmail.com>
2021-05-22 21:14:38 -0700

    recods storage

--- script.js
+++ script.js
@@ -35,7 +35,8 @@
 // }
 
 
-
+var expInDays = 5000;
+var myStorage = window.localStorage;
 
 function btnClick(i) {
 
@@ -64,19 +65,113 @@ function buildPage(content) {
 
 function buildForm(content) {
     var form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", "submit.html");
+    // form.setAttribute("method", "post");
+    // form.setAttribute("action", "./submit.html");
     content.forEach(element => {
-        var elent;
+        var elent = document.createElement(element.field);
 
-        elent = document.createElement(element.field);
         elent.setAttribute("type", element.type);
         elent.setAttribute("name", element.name);
         elent.setAttribute("id", element.name);
         elent.setAttribute("value", element.value);
+
+        if (element.evts != null) {
+            // alert(element.evts.length);
+            element.evts.forEach(evnt => {
+                // alert(evnt.evt);
+                // alert(evnt.callThis);
+                // elent.addEventListener(evnt.evt, evnt.callThis);
+                elent.setAttribute(evnt.evt, evnt.callThis);
+            });
+        }
         form.appendChild(elent);
+
+        form.appendChild(document.createElement("P"));
     });
     var body = document.getElementById('body');
-    body.innerHTML="";
+    body.innerHTML = "";
     body.appendChild(form);
+}
+
+function btn1Click() {
+    // alert("btn1clicked")
+    // alert(document.getElementById('fullName').value);
+    // alert(document.getElementById('age').value);
+    // setCookie(
+    //     'FullName',
+    //     document.getElementById('fullName').value,
+    //     expInDays
+    // );
+    // setCookie(
+    //     'Age',
+    //     document.getElementById('age').value,
+    //     expInDays
+    // );
+    saveData(document.getElementById('fullName').value,
+        document.getElementById('age').value
+    );
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+function saveData(fullname, age) {
+    var id = uuidv4();
+    myStorage.setItem(id, '{'
+        + '"FullName":"' + fullname + '",'
+        + '"Age":' + age
+        + '}');
+    alert('Saved : ' + myStorage.getItem(id));
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
+}
+
+function loadRecords() {
+    var list1 = document.getElementById("recordList");
+    var item = '';
+    for (var i = 0; i < myStorage.length; i++) {
+        // alert(myStorage.getItem(myStorage.key(i)));
+        var k = myStorage.key(i);
+        item += '<li id="' + k + '">'
+            + myStorage.getItem(k)
+            + '</li>'
+    }
+    list1.innerHTML = item;
 }
